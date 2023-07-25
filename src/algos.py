85953cccf0c5ad0234143785f1c923fc2bb1a587
Hamza Shahid <hamzashahid.blit.github@protonmail.com>
2023-07-25 14:24:30 +0500

    Complete Backtracking Algorithm

--- src/algos.py
+++ src/algos.py
@@ -1,82 +1,44 @@
 import copy
 from src.check import *
 
-def backtrack_recursive (board: sboard) -> sboard:
-    x = 0
-    while x < 9:
-        y = 0
-        while y < 9:
-            cell = board[x][y]
-            if cell == 0:
-                for i in range(1, 10):
-                    board[x][y] = i
-                    if check_board(board):
-                        board[x][y] = n
-                        backtrack_recursive(board)
-                        board[x][y] = 0
-                return
-    print_board(board)
-                        
-
 def backtrack (board: sboard) -> sboard:
     new_board = copy.deepcopy(board)
     fixed_cells = get_fixed_cells(board)
     is_backtracking = False
-    i = -1
-    while i < 9:
-        i += 1
-        j = -1
-        while j < 9:
-            j += 1
-            cell = new_board[i][j]
-            if (i, j) in fixed_cells:
-                if is_backtracking:
-                    if i == 0 and j == 0: # No solution...
-                        return None
-                    elif j == 0:
-                        i -= 1
-                        j = 9
-                    else:
-                        j -= 1
-            else: #elif cell == 0 or is_backtracking:
-                is_cell_valid = False
-                if is_backtracking:
-                    print("\nBACKTRACKED (i, j):", i, j)
-                    if new_board[i][j] == 9: # Actually backtrack
-                        print("\nBACKTRACKING (i, j):", i, j)
+    print()
+    print_board(new_board)
+    print()
+    print("Solving...")
+    print()
+    y = 0
+    while y < 9:
+        x = 0
+        while x < 9:
+            if (y, x) not in fixed_cells:
+                while True:
+                    if new_board[y][x] < 9: 
+                        # Try cell till 9
+                        is_backtracking = False
+                        new_board[y][x] += 1
+                    else: 
+                        # Backtrack 
                         is_backtracking = True
-                        new_board[i][j] = 0
-                        if i == 0 and j == 0: # No solution...
-                            return None
-                        elif j == 0:
-                            i -= 1
-                            j = 9
-                        else:
-                            j -= 1
-                        continue
-                    else:
-                        new_board[i][j] += 1
-                    print("\nDONE SOMESHI (i, j):", i, j)
-                    is_backtracking = False
-                    is_cell_valid = check_board(new_board, True)
-                while not is_cell_valid:
-                    if new_board[i][j] == 9: # Actually backtrack
-                        print("\nBACKTRACKING (i, j):", i, j)
-                        is_backtracking = True
-                        new_board[i][j] = 0
-                        if i == 0 and j == 0: # No solution...
-                            return None
-                        elif j == 0:
-                            i -= 1
-                            j = 9
-                        else:
-                            j -= 1
-                        break
-                    new_board[i][j] += 1
-                    is_cell_valid = check_board(new_board, True)
-            print()
-            print_board(new_board)
-            print()
+                        new_board[y][x] = 0
+                        while True:
+                            if y == 0 and x == 0: # No solution (Trying to backtrack 1st row, 1st col)
+                                return None
+                            elif x == 0: # First col
+                                y -= 1
+                                x = 8
+                            else: # Normal case
+                                x -= 1
+                            if (y, x) not in fixed_cells: break
+                    if check_board(new_board, True) or is_backtracking: break
+            if not is_backtracking: x += 1
+        if not is_backtracking: y += 1
+    print()
+    print_board(new_board)
+    print()
     return new_board
 
 def get_fixed_cells (board: sboard) -> [(int, int)]:
--- tests/test_algos.py
+++ tests/test_algos.py
@@ -6,5 +6,5 @@ from tests.boards import *
 class TestAlgos:
     def test_backtrack (self):
         # assert backtrack(problem_boards[0]) is solution_boards[0]
-        assert check_board(backtrack_recursive(easy_board), False) == True
-        # assert check_board(problem_boards[0], True) == True
+        assert check_board(backtrack(easy_board), False) == True
+        assert check_board(problem_boards[0], True) == True
